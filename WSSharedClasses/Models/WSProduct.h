19ee4c20d467f2a9cb1927e41c22be9ca74b337f
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-06-21 02:01:47 +0700

    Add WSProduct model with well-documentatio

--- WSSharedClasses/Models/WSProduct.h
+++ WSSharedClasses/Models/WSProduct.h
@@ -10,4 +10,53 @@
 
 @interface WSProduct : NSObject
 
+/**
+ @TODO
+ Not actually understand, but maybe we can set the related product of each one.
+ */
+@property (nonatomic, strong) NSArray *related_ids;
+
+/**
+ Each product may has many varuations, such as 2 colors of the same product.
+
+ They may also have more different properties.
+ */
+@property (nonatomic, strong) NSArray *variations;
+
+/**
+ Categories of product as list of strings.
+ */
+@property (nonatomic, strong) NSArray *categories;
+
+/**
+ @TODO
+ */
+@property (nonatomic, strong) NSNumber *shippingRequired;
+
+/**
+ This is product id, not actually related with product's SKU.
+ */
+@property (nonatomic, strong) NSNumber *id;
+
+/**
+ Parents of product, maybe empty.
+ */
+@property (nonatomic, strong) NSArray *prarent;
+
+/**
+ This is the regular price, there are another price, called sale price.
+ */
+@property (nonatomic, strong) NSNumber *regularPrice;
+
+/**
+ This weight is used in the shipping method.
+ */
+
+@property (nonatomic, strong) NSNumber *weight;
+
+/**
+ Total sales of each product, include variations of it.
+ */
+@property (nonatomic, strong) NSNumber *total_sales;
+
 @end
