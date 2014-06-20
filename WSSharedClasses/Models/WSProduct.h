7d643caa138f8b3774396210ce591cfa20f3c8af
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-06-21 01:41:15 +0700

    Add Product model

---
+++ WSSharedClasses/Models/WSProduct.h
@@ -0,0 +1,13 @@
+//
+//  WSProduct.h
+//  OrangeFashion
+//
+//  Created by Triệu Khang on 21/6/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSProduct : NSObject
+
+@end
